9dce02a53166b28ed1f0c0736654721c5d662f8b
willamy388720 <willamygt@gmail.com>
2021-02-03 16:21:11 -0300

    Adding Post to Firebase

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ import { name as appName } from './app.json'
 
 import storeConfig from "./src/store/storeConfig";
 
+import axios from "axios";
+axios.defaults.baseURL = 'https://lambe-146b4-default-rtdb.firebaseio.com/'
+
 const store = storeConfig()
 const Redux = () => (
   <Provider store={store}>
