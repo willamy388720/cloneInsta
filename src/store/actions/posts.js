7e65bc9885d6e03748e49dbd2169291dc1f2b7e3
willamy388720 <willamygt@gmail.com>
2021-02-03 21:25:11 -0300

    adding comments in firebase

--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -32,10 +32,24 @@ export const addPost = post => {
 }
 
 export const addComment = payload => {
-  return {
-    type: ADD_COMMENT,
-    payload
+  return dispatch => {
+    axios.get(`/posts/${payload.postId}.json`)
+      .catch(err => console.log(err))
+      .then(res => {
+        const comments = res.data.comments || []
+        comments.push(payload.comment)
+        axios.patch(`/posts/${payload.postId}.json`, { comments })
+          .catch(err => console.log(err))
+          .then(res => {
+            dispatch(fetchPosts())
+          })
+      })
   }
+
+  // return {
+  //   type: ADD_COMMENT,
+  //   payload
+  // }
 }
 
 export const setPosts = posts => {
